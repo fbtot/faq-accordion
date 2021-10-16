10e2b9f65cd18d6503ba8de49ae8359f7909f832
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-16 17:37:54 +0200

    ≈ work on accordion function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const questionEl = document.getElementsByClassName("faq__question");
+const answerEl = document.getElementsByClassName("faq__answer");
 
+// aggiungere la proprietà height direttamente allo stile mel tag html
+for (let i = 0; i < questionEl.length; i++) {
+  const element = questionEl[i];
+  element.addEventListener("click", function () {
+    return element.nextElementSibling.classList.toggle("hide");
+  });
+}
