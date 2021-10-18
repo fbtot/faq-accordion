3edc49a538a791c92f4642847ca4867c14475649
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-18 13:28:30 +0200

    add toggle class and fix offsetHeight
    add class .active on toggle to questions and andwers when the question is clicked and fix a problem with offsetHeight (padding was not included)

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ for (let i = 0; i < questionEl.length; i++) {
   const question = questionEl[i];
   let nextElement = questionEl[i].nextElementSibling;
   question.addEventListener("click", function () {
-    if (nextElement.style.height === 0 + "px" || nextElement.clientHeight === 0) {
+    if (nextElement.style.height === 0 + "px" || nextElement.offsetHeight === 1) {
+      console.log(nextElement.offsetHeight);
       nextElement.style.height = nextElement.scrollHeight + "px";
     } else {
       nextElement.style.height = 0;
     }
+    nextElement.classList.toggle("active");
+    question.classList.toggle("active");
   });
 }
