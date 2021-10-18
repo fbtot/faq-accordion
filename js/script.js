bceb78f53075e49f9ca80d1b3f55c87a17550d33
Filippo Bristot <filippo.bristot@hotmail.it>
2021-10-18 09:15:08 +0200

    accordion script basic functionality

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ const answerEl = document.getElementsByClassName("faq__answer");
 
 // aggiungere la proprietà height direttamente allo stile mel tag html
 for (let i = 0; i < questionEl.length; i++) {
-  const element = questionEl[i];
-  element.addEventListener("click", function () {
-    return element.nextElementSibling.classList.toggle("hide");
+  const question = questionEl[i];
+  let nextElement = questionEl[i].nextElementSibling;
+  question.addEventListener("click", function () {
+    if (nextElement.style.height === 0 + "px" || nextElement.clientHeight === 0) {
+      nextElement.style.height = nextElement.scrollHeight + "px";
+    } else {
+      nextElement.style.height = 0;
+    }
   });
 }
